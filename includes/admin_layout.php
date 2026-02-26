80f67835913be68c65f127c78019b71324d561c7
Prajakta Sarode <psarode821@rku.ac.in>
2026-02-27 02:11:58 +0530

    updates

--- includes/admin_layout.php
+++ includes/admin_layout.php
@@ -29,7 +29,7 @@
 <!-- NAVBAR  -->
  <div class="container-fluid ">
             <nav class="navbar navbar-expand-lg navbar-light bg-white">
-            <a class="navbar-brand" href="guest.php"><img class="guest-nav-image" src="../images/logo.jpeg"/><span class="guest-navbar-heading pt-5" id="navbar-name">Health & Wellness</span></a>
+            <a class="navbar-brand" href="../admin/admin.php"><img class="guest-nav-image" src="../images/logo.jpeg"/><span class="guest-navbar-heading pt-5" id="navbar-name">Health & Wellness</span></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
